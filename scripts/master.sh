262ea284efcff87474dd7669e2dd4bc5712403e4
solidnerd <niclas@mietz.io>
2018-11-09 22:20:08 +0100

    Add kubeconfig to master home
    Signed-off-by: solidnerd <niclas@mietz.io>

--- scripts/master.sh
+++ scripts/master.sh
@@ -19,3 +19,6 @@ systemctl enable docker kubelet
 
 # used to join nodes to the cluster
 kubeadm token create --print-join-command > /tmp/kubeadm_join
+
+mkdir -p "$HOME/.kube"
+cp /etc/kubernetes/admin.conf "$HOME/.kube/config"
